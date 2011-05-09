fbfec3e5ee8651ffaa2ad0bdc088778648917156
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-05-09 13:20:52 +0000

    Nuove simulazioni aggiunte e mainLaunch

--- one_1.4.1/src/runBatch.sh
+++ one_1.4.1/src/runBatch.sh
@@ -1,12 +1,3 @@
-#NUMSEEDS=`wc --lines seeds.txt | egrep -o '[0-9]*'`
-INDEX_FILE="`sed -n '1p' nextValues.txt`"
-INDEX_MOVEMENT="`sed -n '2p' nextValues.txt`"
-echo $INDEX_FILE
-echo $INDEX_MOVEMENT
-
-echo "FilesGenerator.rngSeed = ${i}" > seedSettings.txt;
-echo "MovementModel.rngSeed = ${i}" >> seedSettings.txt;
-
-INDEX_MOVEMENT=$INDEX_MOVEMENT+1
-echo $INDEX_FILE > nextValues.txt;
-echo $INDEX_MOVEMENT >> nextValues.txt;
+java mainLaunch
+./one.sh -b 2 WDM_settings.txt m2mshare_settings.txt seedSettings.txt
+shutdown -h now
