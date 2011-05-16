3285e6093f53277ddfb807cad90604120970d2fb
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-05-16 10:37:36 +0000

    Versione con creazione dei grafici Graphviz delle deleghe. Ancora da fare una versione con deleghe a più livelli

---
+++ one_1.4.1/src/report/DelegationGraphvizReport.java
@@ -0,0 +1,67 @@
+package report;
+
+import core.DTNHost;
+import core.FileEventListener;
+
+public class DelegationGraphvizReport extends Report implements
+		FileEventListener {
+	/** Name of the graphviz report ({@value})*/
+	public static final String GRAPH_NAME = "delegationGraph";
+	
+	@Override
+	public void init() {
+		super.init();
+		write("digraph " + GRAPH_NAME + " {");
+		write("rankdir = LR");
+		setPrefix("\t"); // indent following lines by one tab
+	}
+
+	@Override
+	public void newVirtualFile(DTNHost where, String filehash) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fileRequestDelegated(DTNHost from, DTNHost to, String filehash) {
+		write(from + "->" + to + ";");
+	}
+	
+	
+	public void done() {
+				
+		setPrefix(""); // don't indent anymore
+		write("}");
+		
+		super.done();
+	}
+	
+
+	@Override
+	public void pendingDownloadExpired(DTNHost where, DTNHost requestor,
+			String filehash) {}
+
+	@Override
+	public void pendingDownloadCompleted(DTNHost where, DTNHost requestor,
+			String filehash) {
+		write(where+"[color = green; shape = diamond]");
+	}
+
+	@Override
+	public void downloadFWDExpired(DTNHost where, DTNHost requestor,
+			String filehash) {}
+
+	@Override
+	public void downloadFWDReturned(DTNHost from, DTNHost requestor,
+			String filehash) {
+		write(from + "->" + requestor + "[color = green];");
+	}
+
+	@Override
+	public void dataTransferred(DTNHost from, DTNHost to, int bytes) {}
+
+	@Override
+	public void fileRequestSatisfied(DTNHost where, String filehash,
+			boolean selfSatisfied) {}
+
+}
