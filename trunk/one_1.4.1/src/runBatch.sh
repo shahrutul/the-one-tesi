c4b8f0a6fe59a6bb9cc6a76705d30d58e450c80d
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-05-24 07:37:35 +0000

    simulazione 372156336

--- trunk/one_1.4.1/src/runBatch.sh
+++ trunk/one_1.4.1/src/runBatch.sh
@@ -3,7 +3,10 @@
 
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_fileDivision.txt
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_delega.txt
-./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings_temp.txt seedSettings.txt
+svn commit -m "simulazione 372156336"
+./one.sh -b 3 WDM_settings.txt m2mshare_settings_temp.txt seedSettings2.txt
+svn commit -m "simulazione 2030952567"
 
 shutdown -h +1 "Spegnimento in 60 secondi" &
 zenity --question --title 'Simulazioni finite!' --text 'Premi Ok per spegnere subito o No per interrompere lo spegnimento automatico'
