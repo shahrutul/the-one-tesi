76646b0694afadd31d453755dd1b47dfa1b40f06
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-05-24 11:56:38 +0000

    simulazione 861482464

--- one_1.4.1/src/runBatch.sh
+++ one_1.4.1/src/runBatch.sh
@@ -3,18 +3,18 @@
 
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_fileDivision.txt
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_delega.txt
-./one.sh -b 1 WDM_settings.txt m2mshare_settings_temp.txt seedSettings.txt
-svn commit -m "simulazione 372156336"
-./one.sh -b 3 WDM_settings.txt m2mshare_settings_temp.txt seedSettings2.txt
-svn commit -m "simulazione 2030952567"
+./one.sh -b 3 WDM_settings.txt m2mshare_settings_temp.txt seedSettings.txt
+svn commit -m "simulazione 861482464"
+./one.sh -b 2 WDM_settings.txt m2mshare_settings_temp2.txt seedSettings2.txt
+svn commit -m "simulazione 795809157"
 
-shutdown -h +1 "Spegnimento in 60 secondi" &
-zenity --question --title 'Simulazioni finite!' --text 'Premi Ok per spegnere subito o No per interrompere lo spegnimento automatico'
-if [[ $? == 0 ]] ; then
-        echo "Spegnimento"
-        shutdown -h now
-else
-        echo "NOOO Spegnimento"
-        shutdown -c
-fi
+#shutdown -h +1 "Spegnimento in 60 secondi" &
+#zenity --question --title 'Simulazioni finite!' --text 'Premi Ok per spegnere subito o No per interrompere lo spegnimento automatico'
+#if [[ $? == 0 ]] ; then
+#        echo "Spegnimento"
+#        shutdown -h now
+#else
+#        echo "NOOO Spegnimento"
+#        shutdown -c
+#fi
 
