d5ac90a793a94e652d3f2b3dc8f2146f80e5912a
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-05-24 07:04:19 +0000

    seedSetting deve essere locale

--- trunk/one_1.4.1/src/runBatch.sh
+++ trunk/one_1.4.1/src/runBatch.sh
@@ -3,7 +3,7 @@
 
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_fileDivision.txt
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_delega.txt
-./one.sh -b 1 WDM_settings.txt m2mshare_settings.txt seedSettings.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt
 
 shutdown -h +1 "Spegnimento in 60 secondi" &
 zenity --question --title 'Simulazioni finite!' --text 'Premi Ok per spegnere subito o No per interrompere lo spegnimento automatico'
