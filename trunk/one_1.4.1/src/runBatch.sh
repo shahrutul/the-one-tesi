42b7e8f3ae22ef3342314fba6f05c6caee0de571
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-05-31 07:00:10 +0000

    nuovi risultati con più files

--- trunk/one_1.4.1/src/runBatch.sh
+++ trunk/one_1.4.1/src/runBatch.sh
@@ -3,18 +3,23 @@
 
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_fileDivision.txt
 #./one.sh -b 3 WDM_settings_pendolari.txt m2mshare_test_delega.txt
-./one.sh -b 3 WDM_settings.txt m2mshare_settings_temp.txt seedSettings.txt
-svn commit -m "simulazione 861482464"
-./one.sh -b 2 WDM_settings.txt m2mshare_settings_temp2.txt seedSettings2.txt
-svn commit -m "simulazione 795809157"
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt fileSettings/100.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt fileSettings/150.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt fileSettings/200.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt fileSettings/250.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt fileSettings/300.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt fileSettings/350.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings.txt fileSettings/400.txt
+svn add reports/100files/* reports/150files/* reports/200files/* reports/250files/* reports/300files/* reports/350files/* reports/400files/*
+svn commit -m "nuovi dati con più files"
 
-#shutdown -h +1 "Spegnimento in 60 secondi" &
-#zenity --question --title 'Simulazioni finite!' --text 'Premi Ok per spegnere subito o No per interrompere lo spegnimento automatico'
-#if [[ $? == 0 ]] ; then
-#        echo "Spegnimento"
-#        shutdown -h now
-#else
-#        echo "NOOO Spegnimento"
-#        shutdown -c
-#fi
+shutdown -h +1 "Spegnimento in 60 secondi" &
+zenity --question --title 'Simulazioni finite!' --text 'Premi Ok per spegnere subito o No per interrompere lo spegnimento automatico'
+if [[ $? == 0 ]] ; then
+        echo "Spegnimento"
+        shutdown -h now
+else
+        echo "NOOO Spegnimento"
+        shutdown -c
+fi
 
