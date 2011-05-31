33ad51c49903722ae890753e7f262125d1c1f9f8
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-05-31 07:04:53 +0000

    nuovi settings per i test con più files

--- trunk/one_1.4.1/src/runBatch.sh
+++ trunk/one_1.4.1/src/runBatch.sh
@@ -13,6 +13,16 @@
 svn add reports/100files/* reports/150files/* reports/200files/* reports/250files/* reports/300files/* reports/350files/* reports/400files/*
 svn commit -m "nuovi dati con più files"
 
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings2.txt fileSettings/100.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings2.txt fileSettings/150.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings2.txt fileSettings/200.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings2.txt fileSettings/250.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings2.txt fileSettings/300.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings2.txt fileSettings/350.txt
+./one.sh -b 3 WDM_settings.txt m2mshare_settings.txt seedSettings2.txt fileSettings/400.txt
+svn add reports/100files/* reports/150files/* reports/200files/* reports/250files/* reports/300files/* reports/350files/* reports/400files/*
+svn commit -m "altri nuovi dati con più files"
+
 shutdown -h +1 "Spegnimento in 60 secondi" &
 zenity --question --title 'Simulazioni finite!' --text 'Premi Ok per spegnere subito o No per interrompere lo spegnimento automatico'
 if [[ $? == 0 ]] ; then
