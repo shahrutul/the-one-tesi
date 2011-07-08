33256a9f3ff87c89beadd16cc6852f325be553ca
dany.bony <dany.bony@1faa5009-9573-b880-1816-afbb2cfb841e>
2011-07-08 09:51:14 +0000

    aggiornamento moviemnto ad ogni scan

--- trunk/one_1.4.1/src/report/M2MShareMovementReport.java
+++ trunk/one_1.4.1/src/report/M2MShareMovementReport.java
@@ -5,19 +5,34 @@
 package report;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import core.Coord;
 import core.DTNHost;
 import core.MovementListener;
+import core.Settings;
 import routing.M2MShareRouter;
 
 
 public class M2MShareMovementReport extends Report implements MovementListener {
 	
+	public static final String M2MSHARE_NS = "M2MShareRouter";
+	public static final String DELEGATION_DEPTH_S = "delegationDepth";
+	
 	private HashMap<Coord, Integer> locations;
+	private int delegationDepth;
 	
 	
 	public M2MShareMovementReport() {
+		Settings settings = new Settings(M2MSHARE_NS);
+		
+		if (settings.contains(DELEGATION_DEPTH_S)) {
+			delegationDepth = settings.getInt(DELEGATION_DEPTH_S);
+		}
+		else {
+			delegationDepth = 3;
+		}
+		
 		locations = new HashMap<Coord, Integer>();
 		init();		
 	}
@@ -32,7 +47,7 @@ public class M2MShareMovementReport extends Report implements MovementListener {
 
 	private void stampaPosizione(DTNHost host, Coord location) {
 		int delDepth = getDelegationDepth(host);
-		if(delDepth == -1){
+		if(delDepth == -1 ){
 			return;
 		}		
 		if(locations.get(location) == null || locations.get(location) > delDepth){
@@ -51,10 +66,14 @@ public class M2MShareMovementReport extends Report implements MovementListener {
 
 	@Override
 	public void done() {
-		for(Coord c: locations.keySet()){
-			write(String.format("%.2f\t%.2f",c.getX(),c.getY())+ '\t' + locations.get(c));
-			//write(""+c.getX()+'\t'+c.getY() + '\t' + locations.get(c));
+		for(int depth = delegationDepth; depth >= 0; depth--){
+			for(Coord c: locations.keySet()){
+				if(locations.get(c) != null && locations.get(c).intValue() == depth){
+					write(String.format("%.2f\t%.2f",c.getX(),c.getY())+ '\t' + locations.get(c));					
+				}
+			}
 		}
+		
 		super.done();
 	}
 
